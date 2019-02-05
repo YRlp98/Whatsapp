bc7927e6c62fbca670a225fbe9f6465e16c36a62
YRlp98 <yousef.r1999@gmail.com>
2019-02-05 17:45:34 +0330

    Import and Create SQLLite Database

---
+++ lib/sqlliteProvider/provider.dart
@@ -0,0 +1,29 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class Provider {
+  Database db;
+  String _path;
+
+  Future open({String dbName: 'roocket.db'}) async {
+    var databasesPath = await getDatabasesPath();
+    _path = join(databasesPath, dbName);
+
+    db = await openDatabase(_path, version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute('''
+        create table products ( 
+          id integer primary key autoincrement,
+          user_id integer not null, 
+          title text not null,
+          body text not null,
+          image text not null,
+          price text not null,
+          created_at text not null,
+          updated_at text not null)
+          ''');
+    });
+  }
+
+  Future close() async => db.close();
+}
