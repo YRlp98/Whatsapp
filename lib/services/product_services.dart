80c56c18e22dcc7e83f62cb306e0246ac70fb3c4
YRlp98 <yousef.r1999@gmail.com>
2019-02-09 23:39:47 +0330

    Products will save in SQLLite Database

--- lib/services/product_services.dart
+++ lib/services/product_services.dart
@@ -3,19 +3,20 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:whatsapp/helper.dart';
 import 'package:whatsapp/models/product.dart';
+import 'package:whatsapp/sqlliteProvider/products_provider.dart';
 
 class ProductService {
   static Future<Map> getProducts(int page) async {
     if (await checkConnectionInternet()) {
       return await _getAllProductsFromNetWork(page);
     } else {
-//      return
+      return await _getAllProductsFromSQLLite(page);
     }
   }
 
   static Future<Map> _getAllProductsFromNetWork(int page) async {
     final response =
-    await http.get('http://roocket.org/api/products?page=${page}');
+        await http.get('http://roocket.org/api/products?page=${page}');
 
     if (response.statusCode == 200) {
       var responseBody = json.decode(response.body)['data'];
@@ -27,8 +28,6 @@ class ProductService {
 
       print(products.first.createdAt);
 
-//      await _saveAllProductsIntoSQLLite(products);
-
       return {
         'current_page': responseBody['current_page'],
         'products': products
@@ -39,10 +38,20 @@ class ProductService {
   }
 
   static Future<Map> _getAllProductsFromSQLLite(int page) async {
+    var db = new ProductsProvider();
+    await db.open();
 
+    List<Product> products = await db.paginate(page);
+    await db.close();
+
+    return {'current_page': page, 'products': products};
   }
 
-  static Future<bool> _saveAllProductsIntoSQLLite(List<Product> products) {
-
+  static Future<bool> _saveAllProductsIntoSQLLite(
+      List<Product> products) async {
+    var db = new ProductsProvider();
+    await db.open();
+    await db.insertAll(products);
+    await db.close();
   }
 }
--- lib/sqlliteProvider/products_provider.dart
+++ lib/sqlliteProvider/products_provider.dart
@@ -1,11 +1,39 @@
+import 'package:sqflite/sqflite.dart';
 import 'package:whatsapp/models/product.dart';
 import 'package:whatsapp/sqlliteProvider/provider.dart';
 
-class ProductsProvider extends Provider{
+class ProductsProvider extends Provider {
   String _tableName = 'products';
 
-  Future<Product> insert(Product product) async{
-    product.id = await db.insert(_tableName, product.toMap());
+  Future<Product> insert(Product product,
+      {conflictAlgorithm: ConflictAlgorithm.ignore}) async {
+    product.id = await db.insert(_tableName, product.toMap(),
+        conflictAlgorithm: conflictAlgorithm);
     return product;
+  }
+
+  Future<bool> insertAll(List<Product> products) async {
+    await Future.wait(products.map((product) async {
+      await this.insert(product);
+    }));
+    return true;
+  }
+
+  Future<List<Product>> paginate(int page, {int limit: 8}) async {
+    List<Map> maps = await db.query(_tableName,
+        columns: ['id', 'user_id', 'title', 'body', 'image'],
+        limit: limit,
+        offset: page == 1 ? 0 : (page - 1) * limit);
+
+    if (maps.length > 0) {
+      List<Product> products = [];
+      maps.forEach((product) {
+        if (product != null) {
+          products.add(Product.fromJson(product));
+        }
+      });
+      return products;
+    }
+    return null;
   }
 }
