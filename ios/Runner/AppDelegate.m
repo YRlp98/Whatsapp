990b26a485182bebf4efe5fc96b5f5ce0946f9c9
YRlp98 <yousef.r1999@gmail.com>
2019-02-14 18:37:04 +0330

    Specify API Key for Android and iOS

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,6 +5,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [GMSServices provideAPIKey:@"AIzaSyBUflfO0zoNKB5UA6vmZZo-2rQS8ppY91k"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
