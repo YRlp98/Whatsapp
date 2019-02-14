08e62253b0be99dbee576a52b0354efa8a10356f
YRlp98 <yousef.r1999@gmail.com>
2019-02-14 18:55:10 +0330

    Create Map Screen Class

---
+++ lib/pages/map_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text(
+        'Map',
+        style: TextStyle(fontSize: 20.0),
+      ),
+    );
+  }
+}
