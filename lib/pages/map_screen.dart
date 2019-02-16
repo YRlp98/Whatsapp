38475a5e888788fd528e2ea29e931a6f9f1f9bc0
YRlp98 <yousef.r1999@gmail.com>
2019-02-16 18:05:36 +0330

    Add a RaisedButton to set map location to London

--- lib/pages/map_screen.dart
+++ lib/pages/map_screen.dart
@@ -11,22 +11,43 @@ class MapScreenState extends State<MapScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(15.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//  TODO: Sample
+//    return Padding(
+//      padding: EdgeInsets.all(15.0),
+//      child: Column(
+//        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//        children: <Widget>[
+//          Center(
+//            child: SizedBox(
+//              width: 400.0,
+//              height: 600.0,
+//              child: new GoogleMap(
+//                onMapCreated: _onMapCreated,
+//                initialCameraPosition: CameraPosition(
+//                  target: LatLng(35.715298, 	51.404343),
+//                ),
+//              ),
+//            ),
+//          ),
+//        ],
+//      ),
+//    );
+    return new Scaffold(
+      body: new Stack(
+        alignment: Alignment.bottomCenter,
         children: <Widget>[
-          Center(
-            child: SizedBox(
-              width: 400.0,
-              height: 600.0,
-              child: GoogleMap(
-                onMapCreated: _onMapCreated,
-                initialCameraPosition: CameraPosition(
-                  target: LatLng(35.715298, 	51.404343),
-                ),
-              ),
+          new GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: LatLng(35.715298, 51.404343),
             ),
+          ),
+          new RaisedButton(
+            onPressed: () {
+              mapController.animateCamera(CameraUpdate.newCameraPosition(
+                  const CameraPosition(target: LatLng(51.5074, 0.1278))));
+            },
+            child: new Text('Go to Iran '),
           ),
         ],
       ),
@@ -34,8 +55,9 @@ class MapScreenState extends State<MapScreen> {
   }
 
   void _onMapCreated(GoogleMapController controller) {
-    setState(() {
-      mapController = controller;
-    });
+//    setState(() {
+//      mapController = controller;
+//    });
+    mapController = controller;
   }
 }
