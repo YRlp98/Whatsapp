921067436275743a0270acbd7b1114219e6b158e
YRlp98 <yousef.r1999@gmail.com>
2019-02-19 17:22:47 +0330

    Add and set GoogleMap

--- lib/pages/second_map_screen.dart
+++ lib/pages/second_map_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
 class MapLocationScreen extends StatefulWidget {
@@ -9,6 +10,9 @@ class MapLocationScreen extends StatefulWidget {
 class MapLocationScreenState extends State<MapLocationScreen> {
   Map<String, double> _startLocation;
   Map<String, double> _currentLocation;
+
+  GoogleMapController _mapController;
+  CameraPosition _position;
 
   Location _location = new Location();
   bool _permission = false;
@@ -39,10 +43,45 @@ class MapLocationScreenState extends State<MapLocationScreen> {
 
       location = null;
     }
+
+    setState(() {
+      _startLocation = location;
+      _position = new CameraPosition(
+          target: LatLng(location['latitude'], location['longitude']),
+          zoom: 18);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return null;
+    List<Widget> widget;
+    var size = MediaQuery.of(context).size;
+
+    widget.add(new SizedBox(
+        width: size.width,
+        height: 300,
+        child: _position != null
+            ? new GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: _position,
+                trackCameraPosition: true,
+                myLocationEnabled: true,
+              )
+            : new SizedBox()));
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('User Location'),
+      ),
+      body: new Column(
+        children: widget,
+      ),
+    );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      _mapController = controller;
+    });
   }
 }
