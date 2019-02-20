65143bdd49f8515804c9a5cbc3690391b625733b
YRlp98 <yousef.r1999@gmail.com>
2019-02-20 17:22:41 +0330

    Create ThirdMapScreen Class

---
+++ lib/pages/third_map_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+class SecondMapScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new SecondMapScreenState();
+}
+
+class SecondMapScreenState extends State<SecondMapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
