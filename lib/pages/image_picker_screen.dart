9a413d062f65b583d648bf5ea4d55308c4dd5120
Yousef Roshandel <Yousef.r1999@gmail.com>
2019-04-20 20:55:43 +0430

    Create ImagePickerScreen class

---
+++ lib/pages/image_picker_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ImagePickerScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new ImagePickerScreenState();
+}
+
+class ImagePickerScreenState extends State<ImagePickerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
