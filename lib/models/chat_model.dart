d9eb050a67936de7415a1dad7412e46d417ed28a
Yousef Roshandel <Yousef.r1999@gmail.com>
2019-04-25 20:36:37 +0430

    Fix ChatScreen profile pictures

--- lib/models/chat_model.dart
+++ lib/models/chat_model.dart
@@ -26,55 +26,55 @@ List<ChatModel> dummyData = [
     message: '123',
     time: '04:03',
     avatarUrl:
-        'https://media.licdn.com/dms/image/C5103AQHo7RdwqjRn8g/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=U4W3jpEzBK6XRyqPRhopjTw8AWDx-fIauVBTIai7lSA',
+        'https://scontent-lhr3-1.cdninstagram.com/vp/da4516e0f4f1d372fa062fe7c3a1ac6d/5D3FD951/t51.2885-19/s150x150/52097303_363194517856880_3242228167020118016_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com',
   ),
   ChatModel(
     name: 'Arash Barahmand',
     message: 'سلام D:',
     time: '03:03',
     avatarUrl:
-        'https://media.licdn.com/dms/image/C5603AQFUrwhCUSj0mA/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=c8xecOnBri7-ysl2hGoDZD0rsjlJTrXgKBtHqHwVDtM',
+        'https://scontent-lhr3-1.cdninstagram.com/vp/9195f483a144eab4e96ead06b9a659aa/5D52E4BA/t51.2885-19/s150x150/25012105_316582132179828_4185443383684104192_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com',
   ),
   ChatModel(
     name: 'Aref Mozafari',
     message: 'حسش نیست داداش :(',
     time: '03:03',
     avatarUrl:
-        'https://media.licdn.com/dms/image/C5603AQEDdjcpcC3VXA/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=7NlmusKEk7ZLj_6cAxdixxzZmWy_Cm0qfDyxvBsu1gY',
+        'https://scontent-lhr3-1.cdninstagram.com/vp/93aa9dcad33b7b1401d683d1bfff5a39/5D406507/t51.2885-19/s150x150/52365810_1202344736588670_5595286237050044416_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com',
   ),
   ChatModel(
     name: 'Agha Mahdi',
     message: 'آقا یوسف',
     time: '03:03',
     avatarUrl:
-        'https://media.licdn.com/dms/image/C4E03AQGJYHVJNlmriA/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=LRQ06FOYwjf0ohxYAZ6rFS9YOMPgxnVsEssw2vRDag0',
+        'https://scontent-lhr3-1.cdninstagram.com/vp/e4bca73c4f38cfabd9f0c1cc31e560a3/5D6B05EC/t51.2885-19/s150x150/53262163_411910702691111_5206849381914902528_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com',
   ),
   ChatModel(
     name: 'Sadra Makhmali (Lashi)',
     message: 'پول مارو بده لاشی!',
     time: '03:03',
     avatarUrl:
-        'https://media.licdn.com/dms/image/C5603AQHIK-oHKmTM1w/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=6bRTNq71z3L2DrmNICIaSV2p5wlQ_UXbPBClRaICwtU',
+        'https://scontent-lhr3-1.cdninstagram.com/vp/a9c92f2bc0ca869989b951a651157ed0/5D54139F/t51.2885-19/s150x150/15535265_426617081060673_7313927966163992576_a.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com',
   ),
   ChatModel(
     name: 'Masoud Yousefnejad',
     message: 'احسنت!',
     time: '03:03',
     avatarUrl:
-        'https://media.licdn.com/dms/image/C5603AQG9fG5Jbo5j7g/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=aBoHcvz4H0G8Yop-6dmT1aQYj5SIKopMyX5XJSgQypE',
+        'https://scontent-lhr3-1.cdninstagram.com/vp/bafda16b23995ec8a46b13817d7fd2e0/5D3E9368/t51.2885-19/s150x150/14730690_638269519679701_964862796728631296_a.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com',
   ),
   ChatModel(
     name: 'Morteza Gholami',
     message: ':D',
     time: '02:02',
     avatarUrl:
-        'https://media.licdn.com/dms/image/C5603AQE8MLtB9RJhPg/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=CcgaAVGdemI38wAutVCXAlv6-I0lFk7Y6rhZEOlZIvA',
+        'https://scontent-lhr3-1.cdninstagram.com/vp/cd863fd9d0dedc1b8b8485c6bf11b691/5D6BDFE9/t51.2885-19/s150x150/50622487_372990406815500_8530181550984134656_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com',
   ),
   ChatModel(
     name: 'MD LUTFOR RAHMAN',
     message: 'Khelo from Bangladesh!',
     time: '01:01',
     avatarUrl:
-        'https://media.licdn.com/dms/image/C5103AQFRYIIehonrNw/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=IU5oHq2gqi05ZDaka8NJfsoWbjGqnr4P3nEn47UITS4',
+        'https://scontent-lhr3-1.cdninstagram.com/vp/7ea23d83050bc598b762a7cd03ae5e86/5D3B295F/t51.2885-19/s150x150/23498876_1933100103675579_1551689890687614976_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com',
   ),
 ];
