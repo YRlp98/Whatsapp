6057f4d1e350e330d74e06638ff710440ecfc49c
Yousef Roshandel <Yousef.r1999@gmail.com>
2019-05-02 19:18:10 +0430

    Connect single_product_screen class to Payment gateway

--- lib/pages/single_product_screen.dart
+++ lib/pages/single_product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:whatsapp/models/product.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -29,8 +30,10 @@ class SingleProductScreen extends StatelessWidget {
               new Padding(
                 padding: const EdgeInsets.all(10),
                 child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         new Text(product.title,
                             style: new TextStyle(
@@ -42,13 +45,33 @@ class SingleProductScreen extends StatelessWidget {
                       ],
                     ),
                     new FlatButton(
-                        onPressed: () {},
-                        child: new Text(
-                          'خرید محصول',
-                          style: TextStyle(color: Colors.white),
-                        ))
+                      onPressed: () async {
+                        SharedPreferences prefs =
+                            await SharedPreferences.getInstance();
+
+                        String apiToken = prefs.getString('user.api_toekn');
+//                        String url =
+//                            "http://roocket.org/api/product/buy?product_id=1&api_token=${apiToken}";
+                        String url = "https://zarinp.al/@yrlp98";
+
+                        if (await canLaunch(url)) {
+                          await launch(url);
+                        } else {
+                          throw 'Could not launch $url';
+                        }
+                      },
+                      child: new Text(
+                        'خرید محصول',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: Colors.green,
+                    ),
                   ],
                 ),
+              ),
+              new Padding(
+                padding: const EdgeInsets.only(right: 10, left: 10),
+                child: new Text(product.body),
               )
             ],
           ),
